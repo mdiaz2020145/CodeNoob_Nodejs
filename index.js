cea5dd206d52c78493c58cf0dc36f1d75c0c3f75
Marcelo Diaz <88795578+mdiaz2020145@users.noreply.github.com>
2022-07-31 17:57:48 -0600

    Conexion

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+/*const mongoose = require('mongoose')
 const app = require('./app')
 
 mongoose.Promise = global.Promise;
@@ -6,6 +6,22 @@ mongoose.connect('mongodb://localhost:27017/CodeNoob', { useNewUrlParser: true, 
     console.log('Conexion a la base de datos exitosa')
 
     app.listen(3000, function () {
+        console.log('Corriendo en el puerto 3000')
+    })
+
+    //UsuarioController.registrarSuperAdmin();
+
+}).catch(err => console.log(err))*/
+
+const mongoose = require('mongoose')
+const app = require('./app')
+require('dotenv').config()
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log('Conexion a la base de datos exitosa')
+
+    app.listen(process.env.PORT || 3000, function () {
         console.log('Corriendo en el puerto 3000')
     })
 
